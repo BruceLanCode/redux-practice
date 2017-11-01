3ddcb4c42c03d1a64902ce2b9930bc52652ec0fc
lantuhui <shengxiaobai15@163.com>
2017-11-01 12:15:42 +0800

    create Counter.js
    create Counter.js

---
+++ counter/src/components/Counter.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lantu on 2017/11/1.
+ */
+
+import React,{ Component } from 'react'
+
+export default class extends Component{
+    render(){
+        return(
+            <div>haungqian</div>
+        )
+    }
+}
--- counter/src/index.js
+++ counter/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Counter from './components/Counter'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootEl = document.getElementById('root');
+
+const render = () => ReactDOM.render(
+    <Counter></Counter>,
+    rootEl
+)
+
+render()
+
+
+

ea7190589f202b400e64164c06398f7b4e60e107
lantuhui <shengxiaobai15@163.com>
2017-11-02 10:35:45 +0800

    add actions
    add actions

---
+++ todos/src/actions/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lantu on 2017/11/2.
+ */
+
+let nextTodoId = 0
+
+export const addTodo = (text) => ({
+    type: 'ADD_TODO',
+    id: nextTodoId ++,
+    text
+})
+
+export const setVisibilityFilter = (filter) => ({
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+})
+
+export const toggleTodo = (id) => ({
+    type: 'TOGGLE_TODO',
+    id
+})

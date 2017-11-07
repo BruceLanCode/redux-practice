ad7e19e492d3c4dc7a84f6a61f4da672c63e2766
lantuhui <shengxiaobai15@163.com>
2017-11-07 11:44:56 +0800

    add action and constants
    add action and constants

---
+++ shopping-cart/src/actions/index.js
@@ -0,0 +1,43 @@
+/**
+ * Created by lantu on 2017/11/7.
+ */
+
+import shop from '../api/shop'
+import * as types from '../constants/ActionTypes'
+
+const receiveProducets = products => ({
+    type: types.RECEIVE_PRODUCTS,
+    products
+})
+
+export const getAllProducts = () => dispatch => {
+    shop.getProducts(products => {
+        dispatch(receiveProducets(products))
+    })
+}
+
+const addToCartUnsafe = productId => ({
+    type: types.ADD_TO_CART,
+    productId
+})
+
+export const addToCart = productId => (dispatch,getState) => {
+    if(getState().products.byId[productId].inventory > 0){
+        dispatch(addToCartUnsafe(productId))
+    }
+}
+
+export const checkout = products => (dispatch,getState) => {
+    const { cart } = getState()
+
+    dispatch({
+        type: types.CHECKOUT_REQUEST
+    })
+    shop.buyProducts(products,() => {
+        dispatch({
+            type: types.CHECKOUT_SUCCESS,
+            cart
+        })
+    })
+}
+
